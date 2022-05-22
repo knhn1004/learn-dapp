b09bf610c809fdd5bfa9d9a230c235e298baf281
oliver <kunhan945@gmail.com>
2022-05-23 03:51:58 +0800

    token functionalities

--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -1,7 +1,18 @@
 
 pragma solidity >= 0.5.0;
 
+import './Tether.sol';
+import './RWD.sol';
+
 contract DecentralBank {
 	string public name = 'Decental Bank';
 	address public owner;
+	Tether public tether;
+	RWD public rwd;
+
+	constructor(RWD _rwd, Tether _tether) {
+		tether = _tether;
+		rwd = _rwd;
+		owner = msg.sender;
+	}
 }
