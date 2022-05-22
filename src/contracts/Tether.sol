8189d227dcf371c438ef2d387f2db863e7b561ae
oliver <kunhan945@gmail.com>
2022-05-23 02:59:32 +0800

    approval contract

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -12,7 +12,7 @@ contract Tether {
 		uint value
 	);
 
-	event Approve(
+	event Approval(
 		address indexed _owner,
 		address indexed _spender,
 		uint value
@@ -20,16 +20,38 @@ contract Tether {
 
 	mapping(address => uint256) public balancesOf;
 
+	mapping(address => mapping(address=>uint256)) public allowance;
+
 	constructor() {
 		balancesOf[msg.sender] = totalSupply;
 	}
 
-	function transfer(address _to, uint _value) public  returns (bool success){
+	function transfer(address _to, uint256 _value) public  returns (bool success){
 		require(balancesOf[msg.sender] >= _value);
 
 		balancesOf[msg.sender] -= _value;
 		balancesOf[_to] += _value;
 		emit Transfer(msg.sender, _to, _value);
+		return true;
+	}
+
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+		require(balancesOf[_from] >= _value);
+		require(allowance[_from][msg.sender] >= _value);
+
+		balancesOf[_to] += _value;
+		balancesOf[_from] -= _value;
+
+		// TODO: check if this is correct
+		allowance[_from][msg.sender] -= _value;
+
+		emit Transfer(_from, _to, _value);
+		return true;
+	}
+
+	function approve(address _spender, uint256 _value) public returns (bool success) {
+		allowance[msg.sender][_spender] = _value;
+		emit Approval(msg.sender, _spender, _value);
 		return true;
 	}
 
