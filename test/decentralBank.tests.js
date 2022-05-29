44aa8d5b82fda22686210e19fcdcfd7d06f69139
oliver <kunhan945@gmail.com>
2022-05-30 06:28:30 +0800

    refactor

--- test/decentralBank.tests.js
+++ test/decentralBank.tests.js
@@ -1,15 +1,29 @@
+const web3 = require('web3');
+
 const RWD = artifacts.require('RWD');
 const Tether = artifacts.require('Tether');
 const DecentralBank = artifacts.require('DecentralBank');
 
 require('chai').use(require('chai-as-promised')).should();
 
-contract('decentralBank', accounts => {
+contract('decentralBank', ([owner, customer]) => {
   let tether, rwd;
 
   before(async () => {
+    // load contracts
     tether = await Tether.new();
     rwd = await RWD.new();
+    decentralBank = await DecentralBank.new(rwd.address, tether.address);
+
+    function tokens(num) {
+      return web3.utils.toWei(num, 'ether');
+    }
+
+    // transfer all tokens to decentral bank (1 million)
+    await rwd.transfer(decentralBank.address, tokens('1000000'));
+
+    // transfer 100 mock tethers to customer
+    await tether.transfer(customer, tokens('100'), { from: owner });
   });
 
   describe('Mock Tether Deployment', async () => {
